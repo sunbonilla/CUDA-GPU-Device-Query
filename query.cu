5023537444cf28ef42d67987431f469694c28ea7
sunbonilla <sunbonilla@yahoo.com>
2016-03-27 22:31:16 +1300

    Update query.cu

--- query.cu
+++ query.cu
@@ -8,15 +8,24 @@ void query_GPUs() {
         cudaGetDeviceProperties(&prop, i);
         printf("Device Number: %d\n", i);
         printf("  Device name: %s\n", prop.name);
-        printf("  Memory Clock Rate (KHz): %d\n",
-           prop.memoryClockRate);
-        printf("  Memory Bus Width (bits): %d\n",
-           prop.memoryBusWidth);
+        printf("  Major revision number: %d\n", prop.major);
+        printf("  Minor revision number: %d\n", prop.minor);
+        printf(  "Total shared memory per block: %u\n",  devProp.sharedMemPerBlock);
+        printf("  Total registers per block: %d\n",  devProp.regsPerBlock);
+        printf(  "Warp size: %d\n",  devProp.warpSize);
+        printf("  Maximum memory pitch: %u\n",  devProp.memPitch);
+        printf("  Maximum threads per block: %d\n",  devProp.maxThreadsPerBlock);
+        printf("  Clock rate (KHz): %d\n",  devProp.clockRate);
+        printf("  Memory Clock Rate (KHz): %d\n", prop.memoryClockRate);
+        printf("  Memory Bus Width (bits): %d\n", prop.memoryBusWidth);
         printf("  Total VRAM (Bytes): %u\n",  prop.totalGlobalMem);
+        printf("  Total constant memory (Bytes): %u\n",  devProp.totalConstMem);
         printf("  Number of SMs: %d\n", prop.multiProcessorCount);
         printf("  Peak Memory Bandwidth (GB/s): %f\n",
            2.0*prop.memoryClockRate*(prop.memoryBusWidth/8)/1.0e6);
         printf("  Kernel execution timeout: %s\n",  (prop.kernelExecTimeoutEnabled ? "Yes" : "No"));
+        printf("  Concurrent copy and execution: %s\n",  (devProp.deviceOverlap ? "Yes" : "No"));
+        printf("  Concurrent kernels: %s\n",  (prop.concurrentKernels ? "Yes" : "No"));
     }
 
 }
